32979e5ec9bb0765d3151b5a42494f3587d7d9cc
Denish Borad <denishgithub4253@gmail.com>
2024-11-12 23:09:35 +0530

    app set and make user model

---
+++ music_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    user_id     = models.AutoField(primary_key=True)
+    user_name   = models.CharField(max_length=355)
+    user_email  = models.CharField(max_length=255)
+    user_status = models.BooleanField(default=0)
+    deleted_at  = models.DateTimeField(null=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(null=True)
+
+    class meta:
+        db_table = "user"
