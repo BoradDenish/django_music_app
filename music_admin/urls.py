ac64fb1028d6fe2493f563581460e10b8ba2e2ec
Denish Borad <denishgithub4253@gmail.com>
2025-03-22 08:52:32 +0530

    change admin design and endpoint changes

--- music_admin/urls.py
+++ music_admin/urls.py
@@ -3,9 +3,9 @@ from music_admin import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('admin/login', views.admin_login, name='admin_login'),
-    path('admin/logout', views.admin_logout, name='admin_logout'),
-    path('admin/dashboard', views.admin_dashboard, name='admin_dashboard'),
-    path('admin/songs', views.admin_songs, name='admin_songs'),
-    path('admin/users', views.admin_users, name='admin_users'),
+    path('login', views.admin_login, name='admin_login'),
+    path('logout', views.admin_logout, name='admin_logout'),
+    path('dashboard', views.admin_dashboard, name='admin_dashboard'),
+    path('songs', views.admin_songs, name='admin_songs'),
+    path('users', views.admin_users, name='admin_users'),
 ]
